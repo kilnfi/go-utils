8c49b347e635fa8e9a6419587d3b29c3e9d2de58
Nicolas Maurice <nicolas.maurice.valera@gmail.com>
2022-06-08 17:34:29 +0200

    add command for exec layer

---
+++ cmd/context_test.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"context"
+	"testing"
+
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestContext(t *testing.T) {
+	assert.Equal(t, viper.GetViper(), ViperFromContext(context.TODO()))
+
+	newV := viper.New()
+	ctx := WithViper(context.Background(), newV)
+
+	newV.Set("test", "test")
+	assert.Equal(t, newV, ViperFromContext(ctx))
+}
---
+++ cmd/execution_layer.go
@@ -0,0 +1,90 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+
+	execclient "github.com/skillz-blockchain/go-utils/ethereum/execution/client"
+	jsonrpchttp "github.com/skillz-blockchain/go-utils/net/jsonrpc/http"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+type ethELContext struct {
+	context.Context
+	client *execclient.Client
+}
+
+// NewCmdEthEL creates the `eth-el` command
+func NewCmdEthEL(ctx context.Context) *cobra.Command {
+	ethELCtx := &ethELContext{Context: ctx}
+
+	v := ViperFromContext(ctx)
+
+	cmds := &cobra.Command{
+		Use:   "eth-el SUBCOMMAND",
+		Short: "Commands to interact with Ethereum execution layer node",
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			var err error
+			ethELCtx.client, err = execclient.New(EthELConfigFromViper(v).SetDefault())
+			return err
+		},
+	}
+
+	// Register flags
+	EthELAddrFlag(v, cmds.PersistentFlags())
+
+	cmds.AddCommand(newCmdEthELChainID(ethELCtx))
+	cmds.AddCommand(newCmdEthELBlockNumber(ethELCtx))
+
+	return cmds
+}
+func newCmdEthELChainID(ctx *ethELContext) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "chain-id",
+		Short: "Get execution layer chain ID",
+		RunE: PrintJSON(func(cmd *cobra.Command, args []string) (res interface{}, err error) {
+			return ctx.client.ChainID(ctx)
+		}),
+	}
+
+	return cmd
+}
+
+func newCmdEthELBlockNumber(ctx *ethELContext) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "blocknumber",
+		Short: "Get execution layer chain's head number",
+		RunE: PrintJSON(func(cmd *cobra.Command, args []string) (res interface{}, err error) {
+			return ctx.client.BlockNumber(ctx)
+		}),
+	}
+
+	return cmd
+}
+
+const (
+	ethELAddrFlag     = "eth-el-addr"
+	ethELAddrViperKey = "eth.el-addr"
+	ethELAddrEnv      = "ETH_EL_ADDR"
+)
+
+// EthELAddrFlag register flag for Eth1 node to connect to
+func EthELAddrFlag(v *viper.Viper, f *pflag.FlagSet) {
+	desc := fmt.Sprintf(`JSON-RPC address of the Ethereum execution layer node to connect to.
+	Environment variable: %q`, ethELAddrEnv)
+	f.String(ethELAddrFlag, "", desc)
+	_ = v.BindPFlag(ethELAddrViperKey, f.Lookup(ethELAddrFlag))
+	_ = v.BindEnv(ethELAddrViperKey, ethELAddrEnv)
+}
+
+func GetEthELAddr(v *viper.Viper) string {
+	return v.GetString(ethELAddrViperKey)
+}
+
+func EthELConfigFromViper(v *viper.Viper) *jsonrpchttp.Config {
+	return &jsonrpchttp.Config{
+		Address: GetEthELAddr(v),
+	}
+}
