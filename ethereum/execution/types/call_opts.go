22d56976b3c37bc5fcc2e940a41c721b2ab9dbf1
Nicolas Maurice <nicolas.maurice.valera@gmail.com>
2022-06-08 18:32:23 +0200

    enrich options types

--- ethereum/execution/types/call_opts.go
+++ ethereum/execution/types/call_opts.go
@@ -1,8 +1,10 @@
 package types
 
 import (
+	"context"
 	"math/big"
 
+	gethbind "github.com/ethereum/go-ethereum/accounts/abi/bind"
 	gethcommon "github.com/ethereum/go-ethereum/common"
 )
 
@@ -11,4 +13,13 @@ type CallOpts struct {
 	Pending     bool               // Whether to operate on the pending state or the last known one
 	From        gethcommon.Address // Optional the sender address, otherwise the first account is used
 	BlockNumber *big.Int           // Optional the block number on which the call should be performed
+}
+
+func (opts *CallOpts) ToOpts(ctx context.Context) *gethbind.CallOpts {
+	return &gethbind.CallOpts{
+		Context:     ctx,
+		From:        opts.From,
+		Pending:     opts.Pending,
+		BlockNumber: opts.BlockNumber,
+	}
 }
--- ethereum/execution/types/transact_opts.go
+++ ethereum/execution/types/transact_opts.go
@@ -1,10 +1,15 @@
 package types
 
 import (
+	"context"
 	"math/big"
 
+	gethbind "github.com/ethereum/go-ethereum/accounts/abi/bind"
 	gethcommon "github.com/ethereum/go-ethereum/common"
+	gethtypes "github.com/ethereum/go-ethereum/core/types"
 )
+
+type SignTxFunc func(ctx context.Context, addr gethcommon.Address, tx *gethtypes.Transaction, chainID *big.Int) (*gethtypes.Transaction, error)
 
 // TransactOpts is a set of option to fine tune the creation of a valid Ethereum transaction.
 type TransactOpts struct {
@@ -19,4 +24,25 @@ type TransactOpts struct {
 
 	NoSign bool // Do all transact steps and stops before signing
 	Send   bool // Do all transact steps and send the transaction (can not be true if NoSign is true)
+}
+
+func (opts *TransactOpts) ToOpts(ctx context.Context, chainID *big.Int, signTx SignTxFunc) *gethbind.TransactOpts {
+	return &gethbind.TransactOpts{
+		Context: ctx,
+		Signer: func(addr gethcommon.Address, tx *gethtypes.Transaction) (*gethtypes.Transaction, error) {
+			if opts.NoSign {
+				return tx, nil
+			}
+
+			return signTx(ctx, addr, tx, chainID)
+		},
+		From:      opts.From,
+		Nonce:     opts.Nonce,
+		Value:     opts.Value,
+		GasPrice:  opts.GasPrice,
+		GasFeeCap: opts.GasFeeCap,
+		GasTipCap: opts.GasTipCap,
+		GasLimit:  opts.GasLimit,
+		NoSend:    !opts.Send || opts.NoSign,
+	}
 }
