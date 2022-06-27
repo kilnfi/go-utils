b43de62e3b99a69d46e85e1be75f4a955bd8e3d7
Nicolas Maurice <nicolas.maurice.valera@gmail.com>
2022-06-27 18:32:42 +0200

    fix(keystore): add test on sign tx if key is missing

--- keystore/geth/keystore_test.go
+++ keystore/geth/keystore_test.go
@@ -5,6 +5,7 @@ import (
 	"math/big"
 	"testing"
 
+	gethcommon "github.com/ethereum/go-ethereum/common"
 	gethtypes "github.com/ethereum/go-ethereum/core/types"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -36,4 +37,18 @@ func TestSignTx(t *testing.T) {
 	assert.NotEqual(t, r, big.NewInt(0))
 	assert.NotEqual(t, s, big.NewInt(0))
 	assert.Equal(t, big.NewInt(1), tx.ChainId())
+}
+
+func TestSignTxMissingAddress(t *testing.T) {
+	dir := t.TempDir()
+	keys := New(&Config{
+		Path:     dir,
+		Password: "test-pwd",
+	})
+	tx := gethtypes.NewTx(
+		&gethtypes.DynamicFeeTx{},
+	)
+	_, err := keys.SignTx(context.TODO(), gethcommon.HexToAddress("0x027f72bc0ca063e40577d30d336d52fd7bcc7375"), tx, big.NewInt(1))
+	require.Error(t, err)
+	assert.Equal(t, "no key for address \"0x027f72Bc0CA063E40577D30D336D52Fd7bCC7375\"", err.Error())
 }
