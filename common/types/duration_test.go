804303ab3baae053629f1b94c53dff4af6ef493e
Nicolas Maurice <nicolas.maurice.valera@gmail.com>
2022-07-07 00:52:16 +0200

    fix duration marshal

--- common/types/duration_test.go
+++ common/types/duration_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestMarshal(t *testing.T) {
+func TestUnmarshal(t *testing.T) {
 	tests := []struct {
 		desc string
 
@@ -35,6 +35,54 @@ func TestMarshal(t *testing.T) {
 			err := json.Unmarshal(tt.bytes, dur)
 			require.NoError(t, err, "Unmarshal should not error")
 			assert.Equal(t, tt.expectedDuration, *dur, "Duration should be correct")
+		})
+	}
+}
+
+func TestMarshal(t *testing.T) {
+	tests := []struct {
+		desc string
+
+		// JSON body of the request
+		v            interface{}
+		expectedByte []byte
+		expectedErr  error
+	}{
+		{
+			desc:         "duration",
+			v:            Duration{time.Duration(20)},
+			expectedByte: []byte(`"20ns"`),
+		},
+		{
+			desc: "structPtr",
+			v: struct {
+				Duration *Duration `json:"key"`
+			}{
+				Duration: &Duration{time.Duration(20)},
+			},
+			expectedByte: []byte(`{"key":"20ns"}`),
+		},
+		{
+			desc: "structValue",
+			v: struct {
+				Duration Duration `json:"key"`
+			}{
+				Duration: Duration{time.Duration(20)},
+			},
+			expectedByte: []byte(`{"key":"20ns"}`),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			b, err := json.Marshal(tt.v)
+			if tt.expectedErr != nil {
+				require.Error(t, err, "Marshal should error")
+				assert.Equal(t, tt.expectedErr.Error(), err.Error(), "Error message should be correct")
+			} else {
+				require.NoError(t, err, "Marshal should not error")
+				require.Equal(t, tt.expectedByte, b)
+			}
 		})
 	}
 }
