1646d3d0e896e9ae70bc8a5b0d2ed7d9e212f641
Steve Flanagan <flanagan.s@husky.neu.edu>
2022-08-03 18:00:20 -0400

    add bool for whether gorm logger should be on

--- sql/gorm.go
+++ sql/gorm.go
@@ -3,11 +3,16 @@ package sql
 import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
-func GormOpen(cfg *Config) (*gorm.DB, error) {
+func GormOpen(cfg *Config, gormLoggerOn bool) (*gorm.DB, error) {
+	config := new(gorm.Config)
+	if gormLoggerOn {
+		config.Logger = logger.Default.LogMode(logger.Silent)
+	}
 	return gorm.Open(
 		postgres.Open(cfg.DSN().String()),
-		new(gorm.Config),
+		config,
 	)
 }
